328618b2b23ecc77a3518cf517c67c4df10ffab7
kaigendou <83050807+kaigendou@users.noreply.github.com>
2021-07-18 22:01:14 +0900

    Add files via upload

--- Survay/php/create_prof.php
+++ Survay/php/create_prof.php
@@ -23,36 +23,48 @@
     </div>
     <form>
         <div class="form">
-            <div class="old_div">
+            <div class="old_div">　
                 <div class="txt_div">
                     <span class="prof_icon"><ion-icon name="accessibility-outline"></ion-icon></span>
                     <span class="prof_txt">年齢</span>
                 </div>
                 <ul class="old_ul">
-                    <li>
-                        <input type="radio" name="old" value="old_10" id="before10">
-                        <label for="before10">10代　</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="old" value="old_20" id="after10">
-                        <label for="after10">20代　</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="old" value="old_30" id="before20">
-                        <label for="before20">30代　　</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="old" value="old_40" id="after20">
-                        <label for="after20">40代　</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="old" value="old_50" id="before30">
-                        <label for="before30">50代　</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="old" value="after60" id="after60">
-                        <label for="after60">60歳以上</label>
-                    </li>
+                    <label for="old_1">
+                        <li class="old_1">
+                            <input type="radio" name="old" value="old_10" id="old_1" onclick="old_checked()">
+                            10代　
+                        </li>
+                    </label>
+                    <label for="old_2">
+                        <li class="old_2">
+                            <input type="radio" name="old" value="old_20" id="old_2" onclick="old_checked()">
+                            20代　
+                        </li>
+                    </label>
+                    <label for="old_3">
+                        <li class="old_3">
+                            <input type="radio" name="old" value="old_30" id="old_3" onclick="old_checked()">
+                            30代　　
+                        </li>
+                    </label>
+                    <label for="old_4">
+                        <li class="old_4">
+                            <input type="radio" name="old" value="old_40" id="old_4" onclick="old_checked()">
+                            40代　
+                        </li>
+                    </label>
+                    <label for="old_5">
+                        <li class="old_5">
+                            <input type="radio" name="old" value="old_50" id="old_5" onclick="old_checked()">
+                            50代　
+                        </li>
+                    </label>
+                    <label for="old_6">
+                        <li class="old_6">
+                            <input type="radio" name="old" value="after60" id="old_6" onclick="old_checked()">
+                            60歳以上
+                        </li>
+                    </label>
                 </ul>
             </div>
             <div class="gender">
@@ -61,18 +73,24 @@
                     <span class="prof_txt">性別</span>
                 </div>
                 <ul class="gender_ul">
-                    <li>
-                        <input type="radio" name="gender" value="man" id="man">
-                        <label for="man">男性</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="gender" value="woman" id="woman">
-                        <label for="woman">女性</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="gender" value="other" id="othergender">
-                        <label for="othergender">その他</label>
-                    </li>
+                    <label for="gender_1">
+                        <li class="gender_1">
+                            <input type="radio" name="gender" value="man" id="gender_1">
+                            男性
+                        </li>
+                    </label>
+                    <label for="gender_2">
+                        <li class="gender_2">
+                            <input type="radio" name="gender" value="woman" id="gender_2">
+                            女性
+                        </li>
+                    </label>
+                    <label for="gender_3">
+                        <li class="gender_3">
+                            <input type="radio" name="gender" value="other" id="gender_3">
+                            その他
+                        </li>
+                    </label>
                 </ul>
             </div>
             <div class="blood">
@@ -81,22 +99,30 @@
                     <span class="prof_txt">血液型</span>
                 </div>
                 <ul class="blood_ul">
-                    <li>
-                        <input type="radio" name="blood" value="blood_A" id="A">
-                        <label for="A">A型</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="blood" value="blood_B" id="B">
-                        <label for="B">B型</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="blood" value="blood_AB" id="AB">
-                        <label for="AB">AB型</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="blood" value="blood_O" id="O">
-                        <label for="O">O型</label>
-                    </li>
+                    <label for="blood_1">
+                        <li class="blood_1">
+                            <input type="radio" name="blood" value="blood_A" id="blood_1">
+                            A型
+                        </li>
+                    </label>
+                    <label for="blood_2">
+                        <li class="blood_2">
+                            <input type="radio" name="blood" value="blood_B" id="blood_2">
+                            B型
+                        </li>
+                    </label>
+                    <label for="blood_3">
+                        <li class="blood_3">
+                            <input type="radio" name="blood" value="blood_AB" id="blood_3">
+                            AB型
+                        </li>
+                    </label>
+                    <label for="blood_4">
+                        <li class="blood_4">
+                            <input type="radio" name="blood" value="blood_O" id="blood_4">
+                            O型
+                        </li>
+                    </label>
                 </ul>
             </div>
             <div class="job">
@@ -105,46 +131,66 @@
                     <span class="prof_txt">職業</span>
                 </div>
                 <ul class="job_ul">
-                    <li>
-                        <input type="radio" name="job" value="none" id="none">
-                        <label for="none">働いてない(学生 主婦・主婦 その他)</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="free" id="free">
-                        <label for="free">フリーター</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="service" id="service">
-                        <label for="service">サービス業</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="IT" id="IT">
-                        <label for="IT">IT系</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="medic" id="medic">
-                        <label for="medic">医療・福祉系</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="teach" id="teach">
-                        <label for="teach">教育</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="shipping" id="shipping">
-                        <label for="shipping">運送業</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="const" id="const">
-                        <label for="const">建設業</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="create" id="create">
-                        <label for="create">製造業</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="job" value="other" id="otherjob">
-                        <label for="otherjob">その他</label>
-                    </li>
+                    <label for="job_1">
+                        <li class="job_1">
+                            <input type="radio" name="job" value="none" id="job_1">
+                            学生 主婦・主婦 その他
+                        </li>
+                    </label>
+                    <label for="job_2">
+                        <li class="job_2">
+                            <input type="radio" name="job" value="free" id="job_2">
+                            フリーター
+                        </li>
+                    </label>
+                    <label for="job_3">
+                        <li class="job_3">
+                            <input type="radio" name="job" value="service" id="job_3">
+                            サービス業
+                        </li>
+                    </label>
+                    <label for="job_4">
+                        <li class="job_4">
+                            <input type="radio" name="job" value="IT" id="job_4">
+                            IT系
+                        </li>
+                    </label>
+                    <label for="job_5">
+                        <li class="job_5">
+                            <input type="radio" name="job" value="medic" id="job_5">
+                            医療・福祉系
+                        </li>
+                    </label>
+                    <label for="job_6">
+                        <li class="job_6">
+                            <input type="radio" name="job" value="teach" id="job_6">
+                            教育
+                        </li>
+                    </label>
+                    <label for="job_7">
+                        <li class="job_7">
+                            <input type="radio" name="job" value="shipping" id="job_7">
+                            運送業
+                        </li>
+                    </label>
+                    <label for="job_8">
+                        <li class="job_8">
+                            <input type="radio" name="job" value="const" id="job_8">
+                            建設業
+                        </li>
+                    </label>
+                    <label for="job_9">
+                        <li class="job_9">
+                            <input type="radio" name="job" value="create" id="job_9">
+                            製造業
+                        </li>
+                    </label>
+                    <label for="job_10">
+                        <li class="job_10">
+                            <input type="radio" name="job" value="other" id="job_10">
+                            その他
+                        </li>
+                    </label>
                 </ul>
             </div>
             <div class="submit">
