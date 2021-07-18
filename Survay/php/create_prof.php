32ccf76729d6da2ac68c29d347e1f3d700c5c01d
kaigendou <83050807+kaigendou@users.noreply.github.com>
2021-07-19 01:30:16 +0900

    Add files via upload

--- Survay/php/create_prof.php
+++ Survay/php/create_prof.php
@@ -30,37 +30,37 @@
                 </div>
                 <ul class="old_ul">
                     <label for="old_1">
-                        <li class="old_1">
+                        <li id="old_1_li" >
                             <input type="radio" name="old" value="old_10" id="old_1" onclick="old_checked()">
                             10代　
                         </li>
                     </label>
                     <label for="old_2">
-                        <li class="old_2">
+                        <li id="old_2_li">
                             <input type="radio" name="old" value="old_20" id="old_2" onclick="old_checked()">
                             20代　
                         </li>
                     </label>
                     <label for="old_3">
-                        <li class="old_3">
+                        <li id="old_3_li">
                             <input type="radio" name="old" value="old_30" id="old_3" onclick="old_checked()">
                             30代　　
                         </li>
                     </label>
                     <label for="old_4">
-                        <li class="old_4">
+                        <li id="old_4_li">
                             <input type="radio" name="old" value="old_40" id="old_4" onclick="old_checked()">
                             40代　
                         </li>
                     </label>
                     <label for="old_5">
-                        <li class="old_5">
+                        <li id="old_5_li">
                             <input type="radio" name="old" value="old_50" id="old_5" onclick="old_checked()">
                             50代　
                         </li>
                     </label>
                     <label for="old_6">
-                        <li class="old_6">
+                        <li id="old_6_li">
                             <input type="radio" name="old" value="after60" id="old_6" onclick="old_checked()">
                             60歳以上
                         </li>
@@ -74,19 +74,19 @@
                 </div>
                 <ul class="gender_ul">
                     <label for="gender_1">
-                        <li class="gender_1">
+                        <li id="gender_1_li">
                             <input type="radio" name="gender" value="man" id="gender_1" onclick="gender_checked()">
                             男性
                         </li>
                     </label>
                     <label for="gender_2">
-                        <li class="gender_2">
+                        <li id="gender_2_li">
                             <input type="radio" name="gender" value="woman" id="gender_2" onclick="gender_checked()">
                             女性
                         </li>
                     </label>
                     <label for="gender_3">
-                        <li class="gender_3">
+                        <li id="gender_3_li">
                             <input type="radio" name="gender" value="other" id="gender_3" onclick="gender_checked()">
                             その他
                         </li>
@@ -100,25 +100,25 @@
                 </div>
                 <ul class="blood_ul">
                     <label for="blood_1">
-                        <li class="blood_1">
+                        <li id="blood_1_li">
                             <input type="radio" name="blood" value="blood_A" id="blood_1" onclick="blood_checked()">
                             A型
                         </li>
                     </label>
                     <label for="blood_2">
-                        <li class="blood_2">
+                        <li id="blood_2_li">
                             <input type="radio" name="blood" value="blood_B" id="blood_2" onclick="blood_checked()">
                             B型
                         </li>
                     </label>
                     <label for="blood_3">
-                        <li class="blood_3">
+                        <li id="blood_3_li">
                             <input type="radio" name="blood" value="blood_AB" id="blood_3" onclick="blood_checked()">
                             AB型
                         </li>
                     </label>
                     <label for="blood_4">
-                        <li class="blood_4">
+                        <li id="blood_4_li">
                             <input type="radio" name="blood" value="blood_O" id="blood_4" onclick="blood_checked()">
                             O型
                         </li>
@@ -132,61 +132,61 @@
                 </div>
                 <ul class="job_ul">
                     <label for="job_1">
-                        <li class="job_1">
+                        <li id="job_1_li">
                             <input type="radio" name="job" value="none" id="job_1" onclick="job_checked()">
                             学生 主婦・主婦 その他
                         </li>
                     </label>
                     <label for="job_2">
-                        <li class="job_2">
+                        <li id="job_2_li">
                             <input type="radio" name="job" value="free" id="job_2" onclick="job_checked()">
                             フリーター
                         </li>
                     </label>
                     <label for="job_3">
-                        <li class="job_3">
+                        <li id="job_3_li">
                             <input type="radio" name="job" value="service" id="job_3" onclick="job_checked()">
                             サービス業
                         </li>
                     </label>
                     <label for="job_4">
-                        <li class="job_4">
+                        <li id="job_4_li">
                             <input type="radio" name="job" value="IT" id="job_4" onclick="job_checked()">
                             IT系
                         </li>
                     </label>
                     <label for="job_5">
-                        <li class="job_5">
+                        <li id="job_5_li">
                             <input type="radio" name="job" value="medic" id="job_5" onclick="job_checked()">
                             医療・福祉系
                         </li>
                     </label>
                     <label for="job_6">
-                        <li class="job_6">
+                        <li id="job_6_li">
                             <input type="radio" name="job" value="teach" id="job_6" onclick="job_checked()">
                             教育
                         </li>
                     </label>
                     <label for="job_7">
-                        <li class="job_7">
+                        <li id="job_7_li">
                             <input type="radio" name="job" value="shipping" id="job_7" onclick="job_checked()">
                             運送業
                         </li>
                     </label>
                     <label for="job_8">
-                        <li class="job_8">
+                        <li id="job_8_li">
                             <input type="radio" name="job" value="const" id="job_8" onclick="job_checked()">
                             建設業
                         </li>
                     </label>
                     <label for="job_9">
-                        <li class="job_9">
+                        <li id="job_9_li">
                             <input type="radio" name="job" value="create" id="job_9" onclick="job_checked()">
                             製造業
                         </li>
                     </label>
                     <label for="job_10">
-                        <li class="job_10">
+                        <li id="job_10_li">
                             <input type="radio" name="job" value="other" id="job_10" onclick="job_checked()">
                             その他
                         </li>
@@ -202,5 +202,6 @@
 
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 </body>
 </html>
